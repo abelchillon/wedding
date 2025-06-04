3e6f36299de8e5adcb3ab6732d94c11ff3e32152
ABEL\abelc <achillon@emeal.nttdata.com>
2025-06-04 23:51:55 +0200

    new URL backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,39 +1,20 @@
 export async function getFotosCloudinary() {
-
   const apiUrl =
     process.env.REACT_APP_API_URL || 'https://wedding-jwp3.onrender.com/api';
 
-
-
   const res = await fetch(apiUrl + '/fotos');
-
-
 
   if (!res.ok) throw new Error('No se pudieron obtener las fotos');
 
-
-
   // Si la respuesta no es JSON, lanza error más claro
-
-
 
   const contentType = res.headers.get('content-type');
 
-
-
   if (!contentType || !contentType.includes('application/json')) {
-
     throw new Error(
-
       'El backend no responde con JSON. ¿Está el backend levantado y la URL es correcta?'
-
     );
-
   }
 
-
-
   return res.json();
-
 }
-
